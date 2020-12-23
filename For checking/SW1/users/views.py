fcbe89b17cd19beba738783e07d5c2c4d24e2623
WiZor <anton-port18@rambler.ru>
2020-12-23 16:58:15 +0300

    upd

--- For checking/SW1/users/views.py
+++ For checking/SW1/users/views.py
@@ -1,10 +1,10 @@
 from django.views.generic import CreateView
 from django.urls import reverse_lazy
-from django.contrib.auth.forms import UserCreationForm
+from .forms import CustomUserCreationForm
 
 # Create your views here.
 
 class SignUpView(CreateView):
-    form_class = UserCreationForm
+    form_class = CustomUserCreationForm
     template_name = 'signup.html'
     success_url = reverse_lazy('login')
