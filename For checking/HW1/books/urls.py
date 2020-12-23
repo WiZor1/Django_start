0446531dc9bd7f5e0808264c9e0d9fc5bca4136c
WiZor <anton-port18@rambler.ru>
2020-12-23 21:00:19 +0300

    test for HW1 added

--- For checking/HW1/books/urls.py
+++ For checking/HW1/books/urls.py
@@ -4,5 +4,5 @@ from .views import BooksView, BooksReservedView, BookDetailedView
 urlpatterns = [
     path('', BooksView.as_view(), name='books'),
     path('books_reserved/', BooksReservedView.as_view(), name='books_reserved'),
-    path('book/<int:pk>', BookDetailedView.as_view(), name='book_detailed')
+    path('book/<int:pk>/', BookDetailedView.as_view(), name='book_detailed')
 ]
--- For checking/HW1/journals/urls.py
+++ For checking/HW1/journals/urls.py
@@ -4,5 +4,5 @@ from .views import JournalsView, JournalsReservedView, JournalDetailedView
 urlpatterns = [
     path('', JournalsView.as_view(), name='journals'),
     path('journals_reserved/', JournalsReservedView.as_view(), name='journals_reserved'),
-    path('journal/<int:pk>', JournalDetailedView.as_view(), name='journal_detailed'),
+    path('journal/<int:pk>/', JournalDetailedView.as_view(), name='journal_detailed'),
 ]
