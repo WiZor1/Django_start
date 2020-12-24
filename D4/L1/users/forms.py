b0f053e80c5ac8536924c8b3e120af8cc0b2adcb
WiZor <anton-port18@rambler.ru>
2020-12-24 11:48:53 +0300

    upd

--- D4/L1/users/forms.py
+++ D4/L1/users/forms.py
@@ -4,9 +4,12 @@ from django.contrib.auth.forms import UserCreationForm, UserChangeForm
 class RegularUserCreationForm(UserCreationForm):
     class Meta(UserCreationForm.Meta):
         model = RegularUser
-        fields = UserCreationForm.Meta.fields + ('phone_number', 'postal_code', 'age', 'sex',)
+        # fields = UserCreationForm.Meta.fields + ('phone_number', 'postal_code', 'age', 'sex',)
+        fields = ('username', 'phone_number', 'postal_code', 'age', 'sex',)
+        
 
 class RegularUserChangeForm(UserChangeForm):
-    class Meta(UserCreationForm.Meta):
+    class Meta(UserChangeForm.Meta):
         model = RegularUser
-        fields = UserCreationForm.Meta.fields + ('phone_number', 'postal_code', 'age', 'sex',)
+        # fields = UserCreationForm.Meta.fields + ('phone_number', 'postal_code', 'age', 'sex',)
+        fields = ('username', 'phone_number', 'postal_code', 'age', 'sex',)
